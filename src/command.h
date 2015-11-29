0a9c46b04b299a38247f19166692be16412efcdb
Nullsrc <beallen529@gmail.com>
2015-11-28 19:53:59 -0600

    Remove unused command.c and command.h files

--- src/command.h
+++
@@ -1,9 +0,0 @@
-/*------------------------------------------------------------------------------
-| NuCTex		| command.h
-| Author	| Benjamin E Allen
-| Date		| 23 November, 2015
-|-------------------------------------------------------------------------------
-| Overview	| Prototype declarations for functions used in command handling.
-\-----------------------------------------------------------------------------*/
-
-void callCommand(char*);
