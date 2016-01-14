6a33b19b26a3474f7fae35a788558e0c28623287
Nullsrc <nanosoldierenke@gmail.com>
2016-01-13 20:39:33 -0600

    Move color defines to allow multi-file usage

--- src/io.c
+++ src/io.c
@@ -11,14 +11,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#define ANSI_RED		"\x1b[31m"
-#define ANSI_GREEN		"\x1b[32m"
-#define ANSI_YELLOW		"\x1b[33m"
-#define ANSI_BLUE		"\x1b[34m"
-#define ANSI_MAGENTA	"\x1b[35m"
-#define ANSI_CYAN		"\x1b[36m"
-#define ANSI_RESET		"\x1b[0m"
-
 char command[64]; 	// filewide char array to be used to command parsing
 
 /* printMessage() ---
