0c4431765bbcdc19826068ce34c2de2b2fca22d5
Nullsrc <nanosoldierenke@gmail.com>
2016-01-17 16:47:48 -0600

    Add definition for item structs

---
+++ src/items.h
@@ -0,0 +1,27 @@
+/*------------------------------------------------------------------------------
+| NuCTex	| items.h
+| Author	| Benjamin A - Nullsrc
+| Created	| 17 January, 2016
+| Changed	| 17 January, 2016
+|-------------------------------------------------------------------------------
+| Overview	| Declare item structures used in the code
+\-----------------------------------------------------------------------------*/
+#ifndef NULLSRC_ITEMS_HEADER
+#define NULLSRC_ITEMS_HEADER
+
+typedef struct Item {
+	char* name;
+	char* description;
+	int id;
+	float size;
+	float weight;
+
+	int strength;
+	int agility;
+	int intelligence;
+	int charisma;
+} Item;
+
+typedef struct Inventory {
+	item[26];
+} Inventory;
