94a7ade586a33672ce5fdd3a8aecc2f007ec2433
Nullsrc <nanosoldierenke@gmail.com>
2016-01-24 21:00:31 -0600

    Do some more testing

--- src/actor.c
+++ src/actor.c
@@ -9,5 +9,6 @@
 #include "actor.h"
 
 A_index AIndex = { 
-	.actor[0] = {.name="Nullsrc", .health=100, .strength=10, .agility=10, .intelligence=10, .charisma=10, .actorPos=&LIndex.loc[0], .inv.item[0]=&IIndex.item[0],}
+	.actor[0] = {.name="Nullsrc", .health=100, .strength=10, .agility=10, .intelligence=10, .charisma=10, .actorPos=&LIndex.loc[0], .inv.item[0]=&IIndex.item[0],},
+	.actor[1] = {.name="slime", .health=25, .strength=4, .agility=8, .intelligence=3, .charisma=10, .actorPos=&LIndex.loc[1],}
 };
